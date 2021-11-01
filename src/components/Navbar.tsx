56abfc9e1515005955adf2a1838b0f24078391d1
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-01 12:33:45 +0000

    Custom colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ type NavigationItem = {
   name: string;
   href: string;
   current: boolean;
-}
+};
 
-const navigationItems : NavigationItem[] = [
+const navigationItems: NavigationItem[] = [
   { name: "Explore", href: "#", current: false },
   { name: "Routine", href: "#", current: false },
 ];
@@ -19,14 +19,14 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-citron">
       {({ open }) => (
         <>
           <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-citron-200 hover:text-white hover:bg-citron-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -37,8 +37,12 @@ export default function Navbar() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex w-auto items-center text-xl font-bold text-white rounded-md">
-                  <a className="block lg:hidden" href="#">GLOW</a>
-                  <a className="hidden lg:block" href="#">GLOW</a>
+                  <a className="block lg:hidden" href="#">
+                    GLOW
+                  </a>
+                  <a className="hidden lg:block" href="#">
+                    GLOW
+                  </a>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
@@ -48,8 +52,8 @@ export default function Navbar() {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            ? "bg-citron-700 text-white"
+                            : "text-citron-50 hover:bg-citron-400 hover:text-white",
                           "px-3 py-2 rounded-md text-sm font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}>
@@ -63,7 +67,7 @@ export default function Navbar() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
                   <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <Menu.Button className="bg-citron-700 flex text-sm rounded-full focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
@@ -80,14 +84,14 @@ export default function Navbar() {
                     leave="transition ease-in duration-75"
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95">
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             href="#"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-white" : "",
+                              "block px-4 py-2 text-sm text-citron-700 hover:text-citron"
                             )}>
                             Your Profile
                           </a>
@@ -98,8 +102,8 @@ export default function Navbar() {
                           <a
                             href="#"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-white" : "",
+                              "block px-4 py-2 text-sm text-citron-700 hover:text-citron"
                             )}>
                             Settings
                           </a>
@@ -110,8 +114,8 @@ export default function Navbar() {
                           <a
                             href="#"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-white" : "",
+                              "block px-4 py-2 text-sm text-citron-700 hover:text-citron"
                             )}>
                             Sign out
                           </a>
@@ -133,8 +137,8 @@ export default function Navbar() {
                   href={item.href}
                   className={classNames(
                     item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      ? "bg-citron-900 text-white"
+                      : "text-citron-50 hover:bg-citron-400 hover:text-white",
                     "block px-3 py-2 rounded-md text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        citron: {  
+          DEFAULT: '#91972A',  
+          '50': '#DEE297',  
+          '100': '#D9DE87',  
+          '200': '#CFD567',  
+          '300': '#C4CC47',  
+          '400': '#B0B733',  
+          '500': '#91972A',  
+          '600': '#676B1E',  
+          '700': '#3D3F12',  
+          '800': '#131305',  
+          '900': '#000000'
+        },
+        white: {
+          DEFAULT: "#FDF8ED",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
