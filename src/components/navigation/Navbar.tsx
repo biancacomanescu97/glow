8abd72fd7b2cf5dfef92f81491afa55ea32f41f1
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-04 12:49:16 +0000

    Fine tunned the navbar

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -12,6 +12,7 @@ type NavigationItem = {
 
 const navigationItems: NavigationItem[] = [
   { name: "Explore", href: "/explore/brands", current: false },
+  { name: "Reviews", href: "/routine", current: false },
   { name: "Routine", href: "/routine", current: false },
 ];
 
@@ -21,63 +22,68 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="header" className="fixed top-0 right-0 left-0 z-10">
+    <Disclosure as="header" className="fixed z-10 top-0 right-0 left-0">
       {({ open }) => (
         <>
-          <div className="relative z-2 max-w-full border-t px-8 py-4">
-            <div className="relative z-0 flex align-middle max-w-full">
-              <div className="relative w-4/6 flex flex-nowrap flex-grow-1 flex-shrink-0">
-                <div className="flex items-center sm:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-citron-200 hover:text-citron-10 hover:bg-citron-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-citron-10">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-                <div className="flex flex-1 w-2/6">
-                  <div className="m-0 w-4/6 text-2xl font-bold text-pine-200 rounded-md sm:items-center">
-                    <Link
-                      className="hidden lg:block"
-                      to="/"
-                      aria-current={true ? "page" : undefined}>
-                      GLOW
-                    </Link>
-                    <Link
-                      className="block lg:hidden"
-                      to="/"
-                      aria-current={true ? "page" : undefined}>
-                      GLOW
-                    </Link>
+          <div className="relative z-2 w-full px-8 py-4 mx-auto sm:px-6 lg:px-8">
+            <div className="relative flex z-0 w-full items-center justify-between">
+              {/* Mobile menu button */}
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-pine-200 hover:text-citron-10 hover:bg-pine-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pine-10">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              {/* Header title */}
+              <div className="flex flex-auto items-center justify-center text-2xl font-bold text-pine-200 sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <Link
+                    className="block lg:hidden w-auto"
+                    to="/"
+                    aria-current={true ? "page" : undefined}>
+                    GLOW
+                  </Link>
+                  <Link
+                    className="hidden lg:block w-auto"
+                    to="/"
+                    aria-current={true ? "page" : undefined}>
+                    GLOW
+                  </Link>
                   </div>
-                  <div className="flex-1 hidden text-center sm:block sm:ml-6">
-                    <div className="flex space-x-4">
-                      {navigationItems.map((item) => (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          className={classNames(
-                            item.current
-                              ? "bg-citron-700 text-pine-200"
-                              : "text-pine-200 hover:bg-citron-400 hover:text-pine-200",
-                            "px-3 py-2 rounded-md text-sm font-medium"
-                          )}
-                          aria-current={item.current ? "page" : undefined}>
-                          {item.name}
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
+              </div>
+
+              {/* Navitagation items */}
+              <div className="hidden items-center justify-center sm:block sm:ml-6">
+                <div className="flex flex-1">
+                  {navigationItems.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.href}
+                      className={classNames(
+                        item.current
+                          ? "bg-citron-700 text-pine-200"
+                          : "text-pine-200 hover:bg-citron-400 hover:text-pine-200",
+                        "px-3 py-2 rounded-md text-sm font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}>
+                      {item.name}
+                    </Link>
+                  ))}
                 </div>
-                {/* <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"> */}
-                {/* Profile dropdown */}
-                {/* <User />
-                </div> */}
+              </div>
+
+              {/* Profile dropdown */}
+              <div className="flex items-center justify-end sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:flex-auto">
+                {/* <User /> */}
               </div>
             </div>
+
+            {/* Hide navitagation items when in mobile view*/}
             <Disclosure.Panel className="sm:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigationItems.map((item) => (
