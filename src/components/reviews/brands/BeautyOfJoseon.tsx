6bd611af41b92b1b0ad70716083536e77346575a
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-07 18:46:07 +0000

    Beauty of Joseon

--- src/components/reviews/brands/BeautyOfJoseon.tsx
+++ src/components/reviews/brands/BeautyOfJoseon.tsx
@@ -1,8 +1,160 @@
 export default function BeautyOfJoseon() {
   return (
-    <div className="flex-1 md:px-6 md:py-20 lg:px-8 lg:py-16">
-      <div className="flex align-middle justify-center ">
-        <h1>Beauty of Joseon</h1>
+    <div className="flex-1 md:pt-20 lg:pt-28">
+      <div className="block">
+        <div className="grid grid-cols-2 w-full h-4/5">
+          <div className="">
+            <h1 className="flex justify-center align-middle text-lg text-pine-50 font-bold tracking-widest">
+              CALMING SERUM
+            </h1>
+            <h2 className="flex justify-center align-middle text-xs text-citron-400 tracking-tighter">
+              Green Tea & Panthenol
+            </h2>
+            <div className="text-sm tracking-tighter text-pine-50">
+              <div className="m-20 text-base">
+                <h3 className="font-bold tracking-wide">Benefits</h3>
+                <ul className="list-disc text-sm text-citron text-opacity-50">
+                  <li>hydrates & nourishes</li>
+                  <li>brightens & evens out the complexion</li>
+                  <li>
+                    delivers antioxidants to protect skin against UV damage
+                  </li>
+                </ul>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">
+                    Consistency & Texture
+                  </h3>
+                  <div className="list-disc text-sm mr-16 text-citron text-opacity-50">water-gel</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Fragrance</h3>
+                  <div className="list-disc text-sm mr-16 text-citron text-opacity-50">fragrance-free</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Season</h3>
+                  <div className="list-disc text-sm mr-16 text-citron text-opacity-50">All year round</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Rating</h3>
+                  <div className="list-disc text-sm mr-16 text-citron text-opacity-50">10/10</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            <img
+              className="object-cover md:h-full 2xl:h-4/5 float-right"
+              src="/assets/img/reviews/Beauty of Joseon/green-tea.png"
+              alt="Green tea"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="block h-1/3">
+        <div className="grid grid-cols-2 h-4/5">
+          <div className="overflow-hidden">
+            <img
+              className="object-cover h-full float-right"
+              src="/assets/img/reviews/Beauty of Joseon/propolis.png"
+              alt="Honey"
+            />
+          </div>
+          <div className="">
+            <h1 className="flex justify-center align-middle text-base text-propolis font-bold tracking-widest">
+              GLOW SERUM
+            </h1>
+            <h2 className="flex justify-center align-middle text-xs text-propolis-200 tracking-tighter">
+              Propolis & Niacinamide
+            </h2>
+            <div className="text-sm tracking-tighter text-propolis">
+              <div className="m-20 text-base">
+                <h3 className="font-bold tracking-wide">Benefits</h3>
+                <ul className="list-disc text-sm text-propolis text-opacity-50">
+                  <li>hydrates & nourishes</li>
+                  <li>brightens & evens out the complexion</li>
+                  <li>
+                    delivers antioxidants to protect skin against UV damage
+                  </li>
+                </ul>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">
+                    Consistency & Texture
+                  </h3>
+                  <div className="list-disc text-sm mr-16 text-propolis text-opacity-50">water-gel</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Fragrance</h3>
+                  <div className="list-disc text-sm mr-16 text-propolis text-opacity-50">fragrance-free</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Season</h3>
+                  <div className="list-disc text-sm mr-16 text-propolis text-opacity-50">All year round</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Rating</h3>
+                  <div className="list-disc text-sm mr-16 text-propolis text-opacity-50">10/10</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="block h-1/3">
+        <div className="grid grid-cols-2 h-4/5">
+        <div className="">
+            <h1 className="flex justify-center align-middle text-lg text-ginseng font-bold tracking-widest">
+              REPAIR SERUM
+            </h1>
+            <h2 className="flex justify-center align-middle text-xs text-ginseng-400 tracking-tighter">
+            Ginseng & Snail Mucin
+            </h2>
+            <div className="text-sm tracking-tighter text-ginseng">
+              <div className="m-20 text-base">
+                <h3 className="font-bold tracking-wide">Benefits</h3>
+                <ul className="list-disc text-sm text-ginseng-400 text-opacity-50">
+                  <li>hydrates & nourishes</li>
+                  <li>brightens & evens out the complexion</li>
+                  <li>
+                    delivers antioxidants to protect skin against UV damage
+                  </li>
+                </ul>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">
+                    Consistency & Texture
+                  </h3>
+                  <div className="list-disc text-sm mr-16 text-ginseng-400 text-opacity-50">water-gel</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Fragrance</h3>
+                  <div className="list-disc text-sm mr-16 text-ginseng-400 text-opacity-50">fragrance-free</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Season</h3>
+                  <div className="list-disc text-sm mr-16 text-ginseng-400 text-opacity-50">All year round</div>
+                </div>
+                <div className="mx-20 my-5 text-base">
+                  <h3 className="font-bold tracking-wide">Rating</h3>
+                  <div className="list-disc text-sm mr-16 text-ginseng-400 text-opacity-50">10/10</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            <img
+              className="object-cover h-full float-right"
+              src="/assets/img/reviews/Beauty of Joseon/ginseng.png"
+              alt="Ginseng"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
