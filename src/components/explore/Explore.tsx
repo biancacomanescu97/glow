42c74e820a51a021e1ce6252aa9ac6423b1b3a59
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-09 11:05:59 +0000

    Replaced power houses images

--- src/components/explore/Explore.tsx
+++ src/components/explore/Explore.tsx
@@ -5,44 +5,6 @@ export default function Explore() {
   return (
     <div className="block max-w-full">
       <div className="py-16">
-        <h1 className="flex align-middle justify-center text-propolis-400 text-md">
-          THE ABCs
-        </h1>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
-          {abc.map((item) => (
-            <div className="relative flex align-middle justify-center">
-              <Link to={item.href}>
-                <img
-                  className="object-cover"
-                  src={item.imgUrl}
-                  alt={item.imgAlt}
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div> 
-
-      <div className="pb-16">
-        <h1 className="flex align-middle justify-center text-propolis text-md">
-          POWER HOUSES
-        </h1>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
-          {power.map((item) => (
-            <div className="relative flex align-middle justify-center">
-              <Link to={item.href}>
-                <img
-                  className="object-cover rounded-full"
-                  src={item.imgUrl}
-                  alt={item.imgAlt}
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div> 
-
-      <div className="pb-16">
         <h1 className="flex align-middle justify-center text-propolis text-md">
           CHEMICAL EXFOLIANTS
         </h1>
@@ -63,6 +25,44 @@ export default function Explore() {
           ))}
         </div>
       </div>
+
+      <div className="pb-16">
+        <h1 className="flex align-middle justify-center text-propolis-300 text-md">
+          POWER HOUSES
+        </h1>
+        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+          {power.map((item) => (
+            <div className="relative flex align-middle justify-center">
+              <Link to={item.href}>
+                <img
+                  className="object-cover rounded-full"
+                  src={item.imgUrl}
+                  alt={item.imgAlt}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div> 
+
+      <div className="pb-16">
+        <h1 className="flex align-middle justify-center text-propolis-400 text-md">
+          THE ABCs
+        </h1>
+        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+          {abc.map((item) => (
+            <div className="relative flex align-middle justify-center">
+              <Link to={item.href}>
+                <img
+                  className="object-cover opacity-80"
+                  src={item.imgUrl}
+                  alt={item.imgAlt}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div> 
 
       <div className="pb-16">
         <h1 className="flex align-middle justify-center text-pine-800 text-md">
