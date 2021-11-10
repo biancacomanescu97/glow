f4f1c13c8180ad8bfb1f0405d8c8bf19666aedae
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-10 23:24:26 +0000

    Padding for explore page

--- src/components/explore/Explore.tsx
+++ src/components/explore/Explore.tsx
@@ -3,15 +3,15 @@ import {abc, exfoliants, power, teas} from "./data/ingredients";
 
 export default function Explore() {
   return (
-    <div className="block max-w-full">
-      <div className="py-16">
+    <div className="block max-w-full pt-10">
+      <div className="p-16">
         <h1 className="flex align-middle justify-center text-royal-600 text-md">
           CHEMICAL EXFOLIANTS
         </h1>
         <h2 className="flex animate-pulse-bg-once align-middle justify-center text-royal text-opacity-70 text-sm">
           Not as harsh as they sound
         </h2>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+        <div className="grid grid-col-1 lg:grid-cols-3 lg:p-20">
           {exfoliants.map((item) => (
             <div className="relative flex align-middle justify-center">
               <Link to={item.href}>
@@ -26,11 +26,11 @@ export default function Explore() {
         </div>
       </div>
 
-      <div className="pb-16">
+      <div className="p-16">
         <h1 className="flex align-middle justify-center text-propolis-800 text-md">
           POWER HOUSES
         </h1>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+        <div className="grid grid-col-1 lg:grid-cols-3 lg:p-20">
           {power.map((item) => (
             <div className="relative flex align-middle justify-center">
               <Link to={item.href}>
@@ -45,11 +45,11 @@ export default function Explore() {
         </div>
       </div> 
 
-      <div className="pb-16">
+      <div className="p-16">
         <h1 className="flex align-middle justify-center text-royal-600 text-md">
           THE ABCs
         </h1>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+        <div className="grid grid-col-1 lg:grid-cols-3 lg:p-20">
           {abc.map((item) => (
             <div className="relative flex align-middle justify-center">
               <Link to={item.href}>
@@ -64,14 +64,14 @@ export default function Explore() {
         </div>
       </div> 
 
-      <div className="pb-16">
+      <div className="p-16">
         <h1 className="flex align-middle justify-center text-pine-800 text-md">
           TEAS
         </h1>
         <h2 className="flex animate-pulse-bg-once align-middle justify-center text-pine text-opacity-70 text-sm">
           It's 5 o'clock somewhere
         </h2>
-        <div className="grid grid-col-1 lg:grid-cols-3 gap-10 px-20 py-20">
+        <div className="grid grid-col-1 lg:grid-cols-3 lg:p-20">
           {teas.map((item) => (
             <div className="relative flex align-middle justify-center">
               <Link to={item.href}>
