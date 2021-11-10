470c9338d82f2bdc86f74cb885171fcb0959aa5c
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-11-10 23:15:10 +0000

    Convert disclosure to menu

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Disclosure } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import User from "./user/User";
 
@@ -16,20 +17,20 @@ const navigationItems: NavigationItem[] = [
 
 export default function Navbar() {
   return (
-    <Disclosure as="header" className="absolute z-10 inset-x-0 top-4"> 
+    <Menu as="header" className="absolute z-10 inset-x-0 top-4">
       {({ open }) => (
         <>
           <div className="flex">
             {/* Mobile menu button */}
             <div className="absolute pl-4 flex items-center sm:hidden">
-              <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-royal-700 hover:text-royal-100 hover:bg-royal-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-royal-100">
+              <Menu.Button className="inline-flex items-center justify-center p-2 rounded-md text-royal-700 hover:text-royal-100 hover:bg-royal-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-royal-100">
                 <span className="sr-only">Open main menu</span>
                 {open ? (
                   <XIcon className="block h-6 w-6" aria-hidden="true" />
                 ) : (
                   <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                 )}
-              </Disclosure.Button>
+              </Menu.Button>
             </div>
 
             {/* Header title */}
@@ -49,10 +50,9 @@ export default function Navbar() {
             {/* Navitagation items */}
             <div className="hidden absolute w-full sm:flex justify-center text-sm font-medium text-royal-600">
               {navigationItems.map((item) => (
-                <Link 
+                <Link
                   to={item.href}
-                  className="px-10 py-3 transition duration-500 ease-in-out border-b border-floralwhite-100 hover:border-royal-400"
-                  >
+                  className="px-10 py-3 transition duration-500 ease-in-out border-b border-floralwhite-100 hover:border-royal-400">
                   {item.name}
                 </Link>
               ))}
@@ -60,15 +60,28 @@ export default function Navbar() {
           </div>
 
           {/* Navitagation items in mobile view*/}
-          <Disclosure.Panel className="p-4 min-h-screen text-royal-600 text-base font-medium bg-floralwhite-100">
-            {navigationItems.map((item) => (
-              <Link to={item.href} className="block p-3 hover:text-royal-300">
-                {item.name}
-              </Link>
-            ))}
-          </Disclosure.Panel>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-500"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-300"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95">
+            <Menu.Items className="p-4 min-h-screen text-royal-600 text-base font-medium bg-floralwhite-100">
+              {navigationItems.map((item) => (
+                <Menu.Item>
+                    <Link
+                      to={item.href}
+                      className="block p-3 hover:text-royal-300">
+                      {item.name}
+                    </Link>
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
         </>
       )}
-    </Disclosure>
+    </Menu>
   );
 }
